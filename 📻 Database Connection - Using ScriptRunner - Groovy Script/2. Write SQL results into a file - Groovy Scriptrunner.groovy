85afc42aea1c28468cca7fd2c45dff96127aa933
Rinaldi-James-Michael <64921836+Rinaldi-James-Michael@users.noreply.github.com>
2023-04-08 20:16:55 +0530

    Update 2. Write SQL results into a file - Groovy Scriptrunner.groovy
    Speed upgrade

--- 📻 Database Connection - Using ScriptRunner - Groovy Script/2. Write SQL results into a file - Groovy Scriptrunner.groovy
+++ 📻 Database Connection - Using ScriptRunner - Groovy Script/2. Write SQL results into a file - Groovy Scriptrunner.groovy
@@ -1,61 +1,93 @@
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Created - 24th Jan 2023, 12:03 pm
-//Last Modified - 21st Mar 2023, 04:14 pm
+//Last Modified - 6th April 2023, 06:08 pm
 //Author - Rinaldi Michael
 //References -
 //https://library.adaptavist.com/entity/display-sql-macro
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Results appear the same way a CSV file does. Copy the results into an Excel/Google Sheet and Split Text into Columns using the delimiter provided.
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+   
 import java.io.*
 import java.util.*
 import com.onresolve.scriptrunner.db.DatabaseUtil
 import com.onresolve.scriptrunner.parameters.annotation.*
-  
+import com.onresolve.scriptrunner.parameters.annotation.meta.*
+    
 @ShortTextInput(label = "Resource Name", description = "Type in the name of the Local Database resource set up in ScriptRunner's Reources")
 String resourceName
- 
+   
 @ShortTextInput(label = "SQL Query", description = "Type in the SQL query without the semicolon ; at the end")
 String sqlQuery
- 
+   
 @ShortTextInput(label = "Delimiter", description = "Enter the character that you would like to separate each value with")
 String delimiter
- 
+   
 @ShortTextInput(label = "Script Editor File name", description = "Enter the name of the file. This will be stored in ScriptRunner's Script Editor")
 String fileName
+  
+@Select(
+    label = "Application",
+    description = "Select application as the server's path name is different for both",
+    options = [
+        @Option(label = "Jira", value = "jira"),
+        @Option(label = "Confluence", value = "confluence"),
+    ]
+)
+String application
  
+/////////////////////////////////////////////////////////////////////////////
+   
 def results
 DatabaseUtil.withSql(resourceName)
 { sql ->
     results = sql.rows(sqlQuery)
 }
+   
+/////////////////////////////////////////////////////////////////////////////
  
-String printtext=""
+String printtextHeader=""
 def columnsHeader = results[0].keySet()
- 
+   
 //header
 for(int h=0;h<columnsHeader.size();h++)
 {
-    printtext+=columnsHeader[h]+delimiter
+    printtextHeader+=columnsHeader[h]+delimiter
 }
-printtext+="\n"
+printtextHeader+="\n"
+   
+/////////////////////////////////////////////////////////////////////////////
  
- 
-//values
+//values array
+int sizeOfValues = results.size()
+String[] printtextValues = new String[sizeOfValues];
+int valueCount=0
+   
 for(int r=0;r<results.size();r++)
 {
+    printtextValues[valueCount]=""
     for(int v=0;v<columnsHeader.size();v++)
     {
-        printtext+=results[r].values()[v].toString()+delimiter
+        printtextValues[valueCount]+=results[r].values()[v].toString()+delimiter
     }
-printtext+="\n"
+printtextValues[valueCount]+="\n"
+valueCount++
+}
+  
+  
+  
+//values output
+String outputValues=""
+for(int o=0;o<valueCount;o++)
+{
+    outputValues+=printtextValues[o]
 }
  
-//Write into file
-FileWriter resultFile = new FileWriter("/home/jira/shared_home/scripts/${fileName}.groovy")
-resultFile.write(printtext)
-resultFile.close()
  
+/////////////////////////////////////////////////////////////////////////////
+ 
+   
+//Write into file
+FileWriter resultFile = new FileWriter("/home/${application}/shared_home/scripts/${fileName}.groovy")
+resultFile.write(printtextHeader+outputValues)
+resultFile.close()
+   
 return "The results of the SQL query -><b> ${sqlQuery}</b> has been written into <b>/home/jira/shared_home/scripts/${fileName}.groovy</b>"
