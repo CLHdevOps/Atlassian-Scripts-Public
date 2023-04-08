935e2142cb589be6ea98000faf3c5d1938f5b9c1
Rinaldi-James-Michael <64921836+Rinaldi-James-Michael@users.noreply.github.com>
2023-04-08 20:16:25 +0530

    Update 1. View and copy results of an SQL query - Groovy ScriptRunner.groovy
    Speed upgrade

--- 📻 Database Connection - Using ScriptRunner - Groovy Script/1. View and copy results of an SQL query - Groovy ScriptRunner.groovy
+++ 📻 Database Connection - Using ScriptRunner - Groovy Script/1. View and copy results of an SQL query - Groovy ScriptRunner.groovy
@@ -1,26 +1,27 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Created - Dec 09, 2022 14:06
-//Last Modified - Mar 21, 2023 15:51
+//Last Modified - Apr 06, 2023 18:00
 //Author - Rinaldi Michael
 //References -
 //https://library.adaptavist.com/entity/display-sql-macro
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Results appear the same way a CSV file does. Copy the results into an Excel/Google Sheet and Split Text into Columns using the delimiter provided
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
- 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
 import java.io.*
 import java.util.*
+import java.lang.*
 import com.onresolve.scriptrunner.db.DatabaseUtil
 import com.onresolve.scriptrunner.parameters.annotation.*
-  
+     
 @ShortTextInput(label = "Resource Name", description = "Type in the name of the Local Database resource set up in ScriptRunner's Reources")
 String resourceName
- 
+    
 @ShortTextInput(label = "SQL Query", description = "Type in the SQL query without the semicolon ; at the end")
 String sqlQuery
- 
+    
 @ShortTextInput(label = "Delimiter", description = "Enter the character that you would like to separate each value with")
 String delimiter
+    
+/////////////////////////////////////////////////////////////////////////////
  
 def results
 DatabaseUtil.withSql(resourceName)
@@ -28,26 +29,46 @@ DatabaseUtil.withSql(resourceName)
     results = sql.rows(sqlQuery)
 }
  
-String printtext=""
+/////////////////////////////////////////////////////////////////////////////
+    
+String printtextHeader=""
 def columnsHeader = results[0].keySet()
 //return columns[0]
 //return results[0].values()[1]
- 
+    
 //header
 for(int h=0;h<columnsHeader.size();h++)
 {
-    printtext+=columnsHeader[h]+delimiter
+    printtextHeader+=columnsHeader[h]+delimiter
 }
-printtext+="<br>"
- 
-//values
+printtextHeader+="<br>"
+    
+/////////////////////////////////////////////////////////////////////////////
+  
+//values array
+int sizeOfValues = results.size()
+String[] printtextValues = new String[sizeOfValues];
+int valueCount=0
+   
 for(int r=0;r<results.size();r++)
 {
+    printtextValues[valueCount]=""
     for(int v=0;v<columnsHeader.size();v++)
     {
-        printtext+=results[r].values()[v].toString()+delimiter
+        printtextValues[valueCount]+=results[r].values()[v].toString()+delimiter
     }
-printtext+="<br>"
+printtextValues[valueCount]+="<br>"
+valueCount++
+}
+  
+//values output
+String outputValues=""
+for(int o=0;o<valueCount;o++)
+{
+    outputValues+=printtextValues[o]
 }
  
-return printtext
+/////////////////////////////////////////////////////////////////////////////
+  
+//display the output
+return printtextHeader+outputValues
