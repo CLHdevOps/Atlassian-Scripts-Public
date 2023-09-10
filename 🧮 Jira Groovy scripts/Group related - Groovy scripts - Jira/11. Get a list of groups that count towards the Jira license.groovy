104a6a016669b4a477c1eb6378cf5d656859ce5d
Rinaldi-James-Michael <64921836+Rinaldi-James-Michael@users.noreply.github.com>
2023-09-10 12:16:33 +0530

    Create 11. Get a list of groups that count towards the Jira license.groovy

---
+++ 🧮 Jira Groovy scripts/Group related - Groovy scripts - Jira/11. Get a list of groups that count towards the Jira license.groovy
@@ -0,0 +1,35 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////Rinaldi Michael///////////////////
+/////////Created in 5th April 2023, 06:49 pm///////
+/////Modified in                        ///////////
+//References:                           ///////////
+//https://docs.atlassian.com/software/jira/docs/api/7.1.9/com/atlassian/jira/application/DefaultApplicationRoleManager.html
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        
+import java.lang.String
+import java.io.*
+import com.atlassian.crowd.model.group.Groups
+import com.atlassian.crowd.embedded.api.Group
+import com.atlassian.jira.bc.user.UserService
+import com.atlassian.jira.component.ComponentAccessor
+import com.onresolve.scriptrunner.parameters.annotation.*
+import com.atlassian.jira.bc.user.UserService
+import com.atlassian.jira.user.*
+import com.atlassian.jira.security.login.*
+import com.atlassian.jira.application.*
+    
+ 
+def userManager = ComponentAccessor.userManager
+def groupManager = ComponentAccessor.groupManager
+def loginManager = ComponentAccessor.getComponent(LoginManager)
+def defaultApplicationRoleManager = ComponentAccessor.getComponent(DefaultApplicationRoleManager)
+             
+String printtext="<b>Jira groups that count towards the license are:</b><br><br>"
+ 
+def allLicensedGroupRoles = defaultApplicationRoleManager.getRoles()[1].groups
+for(int l=0;l<allLicensedGroupRoles.size();l++)
+{
+    printtext+="${l+1}. ${allLicensedGroupRoles[l].getName()}<br>"
+}
+    
+return printtext
