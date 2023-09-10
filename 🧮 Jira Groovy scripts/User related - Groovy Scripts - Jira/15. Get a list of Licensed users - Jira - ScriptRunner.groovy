673dfadf7de5bcc11978ab1523a1b993f1733900
Rinaldi-James-Michael <64921836+Rinaldi-James-Michael@users.noreply.github.com>
2023-09-10 12:47:07 +0530

    Update 15. Get a list of Licensed users - Jira - ScriptRunner.groovy
    Auto fetch Licensed Groups

--- 🧮 Jira Groovy scripts/User related - Groovy Scripts - Jira/15. Get a list of Licensed users - Jira - ScriptRunner.groovy
+++ 🧮 Jira Groovy scripts/User related - Groovy Scripts - Jira/15. Get a list of Licensed users - Jira - ScriptRunner.groovy
@@ -1,15 +1,13 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////Rinaldi Michael///////////////////
 /////////Created in 7th July 2023, 02:35pm ////////
-/////Modified in 17th July 2023, 05:50pm///////////
+/////Modified in 10th September 2023, 12:26pm///////////
 //References:                           ///////////
 //https://community.atlassian.com/t5/Jira-Software-questions/To-extract-all-JIRA-users-and-their-last-access-date/qaq-p/1613347
 //https://community.atlassian.com/t5/Jira-Service-Management/get-the-creation-date-of-a-user/qaq-p/802290
-//This script was created for a rest endpoint. Hence, the map output instead of a tabular format. 
-//Groups that count towards the license need to be manually entered in script. 
-//Licensed groups can be found using https://github.com/Rinaldi-James-Michael/Atlassian-Scripts-Public/blob/main/%F0%9F%A7%AE%20Jira%20Groovy%20scripts/Group%20related%20-%20Groovy%20scripts%20-%20Jira/11.%20Get%20a%20list%20of%20groups%20that%20count%20towards%20the%20Jira%20license.groovy
+//This script was created for a rest endpoint. Hence, the map output instead of a tabular format.
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-   
+    
 import java.lang.String
 import java.io.*
 import com.atlassian.crowd.model.group.Groups
@@ -20,22 +18,35 @@ import com.onresolve.scriptrunner.parameters.annotation.*
 import com.atlassian.jira.bc.user.UserService
 import com.atlassian.jira.user.*
 import com.atlassian.jira.security.login.*
-               
+import com.atlassian.jira.application.*
+ 
+                
 //variable declaration for adding groups and projects
 def userManager = ComponentAccessor.userManager
 def groupManager = ComponentAccessor.groupManager
 def loginManager = ComponentAccessor.getComponent(LoginManager)
-               
+                
 //variable delcaration for adding groups to the user
 def userService = ComponentAccessor.getComponent(UserService)
-def userUtil = ComponentAccessor.userUtil                //get all users in Jira and set up the output
+def userUtil = ComponentAccessor.userUtil               
+ 
+//get all users in Jira and set up the output
 def allUsers = ComponentAccessor.getOfBizDelegator().findAll("User");
-def licensedGroups = ['group1','group2','group3','group4'].toSet()
+ 
+//Get licensed groups and users
+def defaultApplicationRoleManager = ComponentAccessor.getComponent(DefaultApplicationRoleManager)
+def allLicensedGroupRoles = defaultApplicationRoleManager.getRoles()[0].groups
+Set<String> licensedGroups = new LinkedHashSet<String>();
+for(int l=0;l<allLicensedGroupRoles.size();l++)
+{
+    licensedGroups.add(allLicensedGroupRoles[l].getName())
+}
 def licensedUsers = groupManager.getNamesOfDirectMembersOfGroups(licensedGroups,allUsers.size())
-  
+   
+//Variable declaration of Array List for output
 List<Map<String , String>> allUsersMap  = new ArrayList<Map<String,String>>();
 int activeUserCounter = 0
-      
+       
 //store results in output variable
 for(int i=0;i<allUsers.size();i++)
 {
@@ -43,10 +54,10 @@ for(int i=0;i<allUsers.size();i++)
     {
         continue;
     }
-  
+   
     if(groupManager.isUserInGroups(userManager.getUserByName(allUsers[i].userName.toString()),licensedGroups)==false)
         continue;
-  
+   
     Map<String,String> user = new HashMap<String, String>();
     try
     {
@@ -70,10 +81,10 @@ for(int i=0;i<allUsers.size();i++)
         user.put("Creation Date",allUsers[i].createdDate.toString())
         user.put("Groups",groupManager.getGroupNamesForUser(allUsers[i].userName.toString()).toString())
     }
-  
-      
+   
+       
     allUsersMap.add(activeUserCounter,user)
     activeUserCounter++
-} 
- 
+}
+  
 return allUsersMap
