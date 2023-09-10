3126254483d6a88a28bf7bc37675507e43a216f0
Rinaldi-James-Michael <64921836+Rinaldi-James-Michael@users.noreply.github.com>
2023-09-10 11:47:35 +0530

    Create 2. Get list and number of active users in specified groups and or email domain (if username is email address)- Jira.groovy

---
+++ 🧮 Jira Groovy scripts/Group related - Groovy scripts - Jira/2. Get list and number of active users in specified groups and or email domain (if username is email address)- Jira.groovy
@@ -0,0 +1,111 @@
+/////////////////Rinaldi Michael///////////////////
+//////Created in 27th January 2023, 08:59 am///////
+/////Modified in 3rd April 2023, 09:17 pm//////////
+///////////////////////////////////////////////////
+    
+import java.lang.String
+import java.io.*
+import com.atlassian.crowd.model.group.Groups
+import com.atlassian.crowd.embedded.api.Group
+import com.atlassian.jira.bc.user.UserService
+import com.atlassian.jira.component.ComponentAccessor
+import com.onresolve.scriptrunner.parameters.annotation.*
+import com.atlassian.jira.bc.user.UserService
+import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.project.Project
+import com.atlassian.jira.security.roles.ProjectRoleManager
+import com.atlassian.jira.bc.projectroles.ProjectRoleService
+import java.lang.String
+import com.atlassian.jira.project.Project
+import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.security.roles.ProjectRoleManager
+import com.atlassian.jira.bc.projectroles.ProjectRoleService
+import com.onresolve.scriptrunner.parameters.annotation.*
+import com.atlassian.jira.user.*
+    
+         
+//variable declaration for adding groups and projects
+def userManager = ComponentAccessor.userManager
+def groupManager = ComponentAccessor.groupManager
+         
+//variable delcaration for adding groups to the user
+def userService = ComponentAccessor.getComponent(UserService)
+def userUtil = ComponentAccessor.userUtil
+         
+    
+//User group input
+@GroupPicker(description = 'Select the group/s from the dropdown.', label = 'Enter the User Groups', multiple = true, placeholder = 'Select group')
+List<Group> groupone
+    
+@ShortTextInput(description = 'Enter a domain like gmail.com, yahoo.com ....etc. This will include users not added to the group but has the domain and is active. Number of groups input here should be the same as the number of groups set in the first field.', label = 'Enter the domain (Required*)')
+String domainInput
+String[] domains
+if(domainInput!=null)
+    domains = domainInput.split(",")
+     
+def loggedInUser = ComponentAccessor.jiraAuthenticationContext.loggedInUser
+     
+def usersFromGroup = userUtil.getAllUsersInGroups(groupone)
+def allUsers = userManager.getAllApplicationUsers()
+    
+String printtext
+String[] groupName = new String[groupone.size()]
+    
+for(int g=0;g<groupone.size();)
+{
+    groupName[g] = groupone[g].getName()
+    g++
+}
+printtext="<h1><b>Active users from the group/s -> ${groupName}</h1></b><br><br>"
+    
+int userCount = 1
+    
+//Users in specified group
+int inGroup=0
+for(int i=0;i<usersFromGroup.size();)
+{
+    if(userManager.getUserByKey(usersFromGroup[i].getKey()).isActive()==true)
+    {
+        printtext=printtext+"${userCount}. "+usersFromGroup[i].getDisplayName()+"*"+usersFromGroup[i].getEmailAddress()+"<br>"
+        userCount++
+        inGroup++
+    }
+    i++
+}
+   
+   
+//Users not added to the group but has the domain and is active
+int notInGroup=0
+printtext+="<h1><b>Active users from domain/s who are not part of the OpCo group-> ${domainInput}</h1></b><br><br>"
+for(int j=0;j<allUsers.size();)
+{
+        String[] domain = allUsers[j].getEmailAddress().split('@')
+        if(allUsers[j].getEmailAddress()=="")
+            domain = allUsers[j].getUsername().split('@')
+ 
+        //if(allUsers[j].getUsername().contains('mailto'))
+        //    return allUsers[j].getEmailAddress()==null
+ 
+        for(int d=0;d<domains.size();)
+        {
+            try
+            {
+            if (domain.contains(domains[d]) && userManager.getUserByName(allUsers[j].getName()).isActive()==true && groupManager.isUserInGroup(userManager.getUserByName(allUsers[j].getName()),groupone[d])==false)
+            {
+            printtext=printtext+"${userCount}. "+allUsers[j].getDisplayName()+"*"+allUsers[j].getEmailAddress()+"<br>"
+            userCount++
+            notInGroup++
+            }
+            }
+            catch(Exception ex)
+            {
+                //ignore
+            }
+        d++
+        }
+j++
+}//end of last for loop
+   
+    
+    
+return "Number of users - ${userCount-1}. <br>${inGroup} users are assigned to the OpCo group. ${notInGroup} users are not in the OpCo group."+printtext
